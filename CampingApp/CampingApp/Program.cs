167ef416e9ea949a8d35f14c291269b578064c4d
Izabela <izabela.tarasin@gmail.com>
2022-05-09 19:23:53 +0200

    initial implementation

--- CampingApp/CampingApp/Program.cs
+++ CampingApp/CampingApp/Program.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CampingApp;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Components.Authorization;
+using App_Camping.Data;
+using CampingApp.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(
+    sp => new HttpClient {
+        BaseAddress = new Uri("https://localhost:3001")
+    });
 
 await builder.Build().RunAsync();
 
