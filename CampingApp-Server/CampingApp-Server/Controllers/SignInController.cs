82e71ad18f8f9a1d077b372257d420452b8e66c6
Izabela <izabela.tarasin@gmail.com>
2022-05-13 19:17:23 +0200

    added initial logic for signIn

---
+++ CampingApp-Server/CampingApp-Server/Controllers/SignInController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CampingApp_Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampingApp_Server.Controllers
+{
+	public record SignInDTO(string email, string password);
+
+	[ApiController]
+	[Route("[controller]")]
+	public class SignInController : ControllerBase
+	{
+		private IUserService _userService;
+
+		public SignInController(IUserService userService)
+		{
+			_userService = userService;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SignIn(SignInDTO dto)
+		{
+			//wywolanie metody do logowania uzytkownika
+			var result = await _userService.SignIn(dto.email, dto.password);
+			//korzystam z usermanager
+			if (result == null)
+			{
+				return BadRequest("Logowanie nie powiodło się");
+			}
+			return Ok("Logowanie przebiegło pomyślnie");
+
+		}
+	}
+}
+
